04320b65faa808980876c9da1a3a8f92f87cbee8
Sri Saleela Karumudi <126276607+SriSaleela@users.noreply.github.com>
2023-06-01 11:07:07 +0200

    Update Flight1_fare.py

--- Flight1_fare.py
+++ Flight1_fare.py
@@ -3,13 +3,13 @@ st.title('Flight Fare Prediction')
 
 import pickle
 import pandas as pd
-model = pickle.load(open('reg_rfmodel.sav', 'rb'))
+#model = pickle.load(open('reg_rfmodel.sav', 'rb'))
 
-new_data = pd.DataFrame({
-    'Source':[Mumbai],
-    'Destination':[Hyderabad],
-    'Total_Stops':[2]
+#new_data = pd.DataFrame({
+  #  'Source':[Mumbai],
+   # 'Destination':[Hyderabad],
+  #  'Total_Stops':[2]
     
-})
-prediction = model.predict(new_data)
-st.write("The price of the flight is:", prediction)
+#})
+#prediction = model.predict(new_data)
+#st.write("The price of the flight is:", prediction)
