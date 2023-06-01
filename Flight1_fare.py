fc29cb0c5049a0c94ed2b586baa4548c35fd154d
Sri Saleela Karumudi <126276607+SriSaleela@users.noreply.github.com>
2023-06-01 09:19:25 +0200

    Add files via upload

--- Flight1_fare.py
+++ Flight1_fare.py
@@ -3,12 +3,13 @@ st.title('Flight Fare Prediction')
 
 import pickle
 import pandas as pd
-model = pickle.load(open('reg_rfmodel.sav', 'rb'))
+model = pickle.load(open('reg_rfmodel.sav.cpgz', 'rb'))
 
 new_data = pd.DataFrame({
-    'Source':[Bangalore],
-    'Destination':[Delhi],
+    'Source':[Mumbai],
+    'Destination':[Hyderabad],
     'Total_Stops':[2]
+    
 })
 prediction = model.predict(new_data)
 st.write("The price of the flight is:", prediction)
